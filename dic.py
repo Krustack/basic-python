d0439aa58a422ac5c30bd204ee862b29f2ce86da
Wisanu <wisanu.pho@cru.ac.th>
2022-06-14 14:00:33 +0700

    edit dic

--- dic.py
+++ dic.py
@@ -2,5 +2,13 @@ car = {"brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-print(car['brand'])
+print(car["brand"])
+print(car)
+x = car.keys()
+print(x)
+#เพิ่มค่าในdic
+car["color"] = "white"
+print(car)
+#แก้ค่าในdic
+car["color"] = "red"
 print(car)
