0a458e6fc7bfee662aad79788359a084802686c3
Wisanu <wisanu.pho@cru.ac.th>
2022-06-21 13:55:30 +0700

    'Loop'

---
+++ loop-condition.py
@@ -0,0 +1,10 @@
+for i in range(1,11,1):
+    print(i)
+for j in range(5):
+    print(j)
+List1=['Hello','Hello','Hedeyang','Yoo','Nor','Emea']
+for e in List1:
+    print(e)
+Dic = {'name':'Tuu','Job':'soldier','age':'25'}
+for key,value in Dic.items():
+    print(key,value)
